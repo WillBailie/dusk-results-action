7f426a351012ea3b478337665c7af69c904a2bc2
Will <willb@openplay.co.uk>
2023-08-03 11:45:33 +0100

    Updated default action

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+const core = require('@actions/core');
+const github = require('@actions/github');
+const data = require('./dusk_output.json')
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    core.warning('test warning');
-    console.log(`The event payload: ${payload}`);
+      var testA = JSON.parse(JSON.stringify(data));
+      // testA.forEach(element => {
+        // console.log(element.name);
+      // });
+      core.error(element.name)
   } catch (error) {
     core.setFailed(error.message);
   }
