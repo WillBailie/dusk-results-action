de0521497259f1777549f59621db6a199d4ecae7
Will <willb@openplay.co.uk>
2023-08-03 10:22:11 +0100

    added main.yml

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const core = require('@actions/core')
-const core = require('@actions/github')
+const github = require('@actions/github')
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    core.warning('test warning');
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
