f6ce2d59a2c0f5db32615287d62b5655e44a5d6c
Will <willb@openplay.co.uk>
2023-08-04 16:19:57 +0100

    fixed index.js file

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const data = require('./dusk_output.json');
-const { Octokit } = require("@octokit/rest");
 
 try {
-      var testA = JSON.parse(JSON.stringify(data));
-      // testA.forEach(element => {
-        // console.log(element.name);
-      // });
-      core.error(element.name)
-      
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  data.forEach(element => {
+    console.log(element.name);
+  });
+
+} catch (error) {
+  core.setFailed(error.message);
+}
