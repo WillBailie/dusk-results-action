d09176b5e6154989a2fffba77674715e1ca11177
Will <willb@openplay.co.uk>
2023-08-07 09:44:52 +0100

    added check request

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const json = require('./dusk_output.json')
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+if (json) {
+  core.info('passed');
+} else {
+
+  core.setFailed('failed');
+
+  const token = core.getInput('github_token')
+  const octokit = new github.getOctokit(token)
+
+  const check = await octokit.rest.checks.create({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    name: 'json',
+    head_sha: github.context.sha,
+    status: 'completed',
+    conclusion: 'failure',
+    output: {
+      title: 'test title',
+      summary: 'test summary',
+      annotations: [
+        {
+          path: 'testpath',
+          start_line: 1,
+          end_line: 1,
+          annotation_level: 'failure',
+          message: 'testMessage',
+          start_column: 1,
+          end_column: 1
+        }
+      ]
+    }
+  });
+}
