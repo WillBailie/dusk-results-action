c6b84212f246e71523e61d33e507d5173203a521
Will <willb@openplay.co.uk>
2023-08-09 10:21:53 +0100

    updated github token

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function run() {
     }
 
     if (annotations.length > 0) {
-      const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+      const octokit = github.getOctokit(core.getInput('github_token'));
       await octokit.checks.create({
         ...github.context.repo,
         name: 'Generate annotations',
