8f1e13fe8d8efd426a774b5666e8444cb10492c2
Will <willb@openplay.co.uk>
2023-08-09 12:26:00 +0100

    added check for file

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 async function run() {
   try {
     const annotations = [];
     const jsonData = JSON.parse(core.getInput('report_path'));
-    
-    // Loop through JSON items and check for the required property
-    for (const item of jsonData) {
+
+    if (doesFileExist(jsonData)) {
+      // Loop through JSON items and check for the required property
+      for (const item of jsonData) {
         annotations.push({
           title: item.title,
           message: item.message,
@@ -16,25 +18,37 @@ async function run() {
           end_line: item.line,
           annotation_level: item.annotation_level,
         });
-    }
+      }
 
-    if (annotations.length > 0) {
-      const octokit = github.getOctokit(core.getInput('github_token'));
-      await octokit.rest.checks.create({
-        ...github.context.repo,
-        name: 'Generate annotations',
-        head_sha: github.context.sha,
-        status: 'completed',
-        conclusion: 'failure',
-        output: {
-          title: 'Check JSON Action',
-          summary: 'The JSON check found issues.',
-          annotations,
-        },
-      });
+      if (annotations.length > 0) {
+        const octokit = github.getOctokit(core.getInput('github_token'));
+        await octokit.rest.checks.create({
+          ...github.context.repo,
+          name: 'Generate annotations',
+          head_sha: github.context.sha,
+          status: 'completed',
+          conclusion: 'failure',
+          output: {
+            title: 'Check JSON Action',
+            summary: 'The JSON check found issues.',
+            annotations,
+          },
+        });
+      } else {
+        core.setFailed('File does not exist');
+      }
     }
   } catch (error) {
   }
 }
 
 run();
+
+function doesFileExist(filePath) {
+  try {
+    fs.accessSync(filePath, fs.constants.F_OK);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
