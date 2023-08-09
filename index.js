fbd8af9a6878035e424915a5926d063a5e557db8
Will <willb@openplay.co.uk>
2023-08-09 16:04:52 +0100

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 const fs = require('fs');
 
 const input = core.getInput('report_path');
-const content = fs.readFile(input, { encoding: 'utf8' });
+const content = fs.readFile(input);
 console.log(JSON.parse(content));
 
 async function run() {
